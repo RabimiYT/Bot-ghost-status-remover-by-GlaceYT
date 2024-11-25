8f9e8609333a1080bea6818b483d71e13852fac0
RabimiYT <fakerabimi@gmail.com>
2024-11-25 09:51:08 +0900

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["暇だ", "ラビミだよ"];
+const statusMessages = ["暇だ", "ラビミだよ", "マイクラ楽しいなぁ", "('3')", "ラビミは天才"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
